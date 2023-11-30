c0e27b0318134c0afd08e5220b0f01cf05a92981
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2023-11-30 14:44:44 +0100

    Add Properties project

--- Fundamentos de Swift/Properties/Properties/main.swift
+++ Fundamentos de Swift/Properties/Properties/main.swift
@@ -1,17 +1,20 @@
 //
 //  main.swift
-//  Basics
+//  Properties
 //
-//  Created by Arturo Carretero Calvo on 29/11/23.
+//  Created by Arturo Carretero Calvo on 30/11/23.
 //
 
 import Foundation
 
 let name = "Arturo"
+
 print(name)
 
 var lastname = "Carretero"
+
 print(lastname)
 
 lastname = "Carretero Calvo"
+
 print(lastname)
